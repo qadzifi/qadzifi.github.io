7e0ad91852486a03b4497a2f5db0f60da1f3adcb
Qadzif Kamil Zahari <qadzifi.public@gmail.com>
2022-11-02 17:18:42 +0700

    fix: change front page title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Frontier Page';
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
